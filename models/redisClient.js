3785522c89ea864bffc7c6b689f661a3aa36865a
liujb <liujiangbei@126.com>
2015-08-10 12:57:05 +0800

    modify

--- models/redisClient.js
+++ models/redisClient.js
@@ -1,14 +1,14 @@
 var redis = require("redis");
 var redisConf = require("../conf/redis");
 
-//
-var client = redis.createClient(redisConf.port, redisConf.host);
+// client settings
+var client = redis.createClient(redisConf.port, redisConf.host, {no_ready_check: true});
 
 /**
  * Monitor redis error event
  */
 client.on("error", function (err) {
-  console.log("Error " + err);
+  console.log(err);
 });
 
 module.exports = client;
