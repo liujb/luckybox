9509d17acc287b7b3b0c7ccc0d57672b26e7ce84
liujb <liujiangbei@126.com>
2015-08-13 23:22:57 +0800

    modify

--- conf/redis.js
+++ conf/redis.js
@@ -1,13 +1,13 @@
 // development
-//var redis = {
-//  host: '127.0.0.1',
-//  port: 6379
-//};
+var redis = {
+  host: '127.0.0.1',
+  port: 6379
+};
 
 // production
-var redis = {
-  host: '10.242.151.144',
-  port: 3006
-};
+//var redis = {
+//  host: '10.242.151.144',
+//  port: 3006
+//};
 
 module.exports = redis;
